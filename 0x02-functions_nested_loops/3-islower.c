b932737477033122491d85abf111856211111105
Benazir Ali <ben99first@gmail.com>
2022-07-06 15:34:27 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "mian.h"
+
+/**
+ * _islower - check if lower case
+ * @c: char param
+ *
+ * Description: returns one for lowercase
+ *
+ * Return: 0 for uppercase, 1 for lowercase
+ */
+int _islower(int c)
+
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		retrun (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
