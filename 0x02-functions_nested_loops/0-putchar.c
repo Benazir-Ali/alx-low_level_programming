0937ecb8878fd2df88cc10974b8ab485775dedbd
Benazir Ali <ben99first@gmail.com>
2022-07-06 14:51:46 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - main block
+ * Description: prints _putchar
+ * Return: 0 Success
+ */
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchat('\n');
+
+	return (0);
+
+}
