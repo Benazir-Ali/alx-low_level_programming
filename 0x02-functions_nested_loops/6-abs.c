b9e70ebfcb11ad6f0e4c41497be833c637f1c851
Benazir Ali <ben99first@gmail.com>
2022-07-06 16:00:04 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute val
+ * @n: int param
+ *
+ * Description: prints absolute value
+ * Return: 1 if positive
+ */
+int _abs(int n)
+
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+
+}
