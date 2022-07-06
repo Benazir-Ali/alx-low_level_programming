6ebe4c53146f3c655b15775fd397163da73b36bb
Benazir Ali <ben99first@gmail.com>
2022-07-06 16:06:09 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#iclude "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: int param
+ *
+ * Description: prints last digit
+ * Return: last digit
+ */
+int print_last_digit(int )
+
+{
+	int ln = n % 10;
+
+	if (n < 0)
+		ln = ln* -1;
+	_putchar(ln + '0')
+	return (ln);
+
+}
