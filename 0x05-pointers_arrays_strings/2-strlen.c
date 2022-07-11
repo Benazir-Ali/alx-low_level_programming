6228aba727987280806f23c2fd26b24a8b6331df
Benazir Ali <ben99first@gmail.com>
2022-07-11 14:33:15 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - len of str
+ * @s: param
+ *
+ * Return: str len
+ */
+int _strlen(char *s)
+
+{
+	int count = 0;
+
+	while(*(s + count) != '\0')
+		count ++;
+
+	return (0);
+}
